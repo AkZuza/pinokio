31aa99be4635a1f9adcbd52eee22cede878fb3fc
Akhil Jose <akhiljose@gmail.com>
2024-10-04 18:22:08 +0530

    implement page with search bar

--- lib/pages/diary_page.dart
+++ lib/pages/diary_page.dart
@@ -15,8 +15,42 @@ class DiaryPage extends StatefulWidget {
 class _DiaryState extends State<DiaryPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Diary"),
-    );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            // Main content of your screen
+            Expanded(child: Center(child: Text('Main Content'))),
+
+            // Bottom row container
+            Container(
+              height: 60, // Adjust height as needed
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: TextField(
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          hintText: 'Enter text here',
+                          contentPadding: EdgeInsets.only(left: 30.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.send),
+                    color: Colors.brown,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
