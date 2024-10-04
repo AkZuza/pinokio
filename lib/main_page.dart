64650cc0edbb7c34b303f470f88110efb759def8
Akhil Jose <akhiljose@gmail.com>
2024-10-04 17:23:23 +0530

    create mainpage with navigation

---
+++ lib/main_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:pinokio/navigation/destinations.dart';
+import 'package:pinokio/navigation/ui/main_nav_bar.dart';
+import 'package:pinokio/pages/checklist_page.dart';
+import 'package:pinokio/pages/diary_page.dart';
+import 'package:pinokio/pages/notes_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int selectedIndex = 1;
+
+  void updateSelectedIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  final List appBars = [notesAppBar, diaryAppBar, checklistAppBar];
+  final List screens = [NotesPage(), DiaryPage(), ChecklistPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: screens[selectedIndex],
+      appBar: appBars[selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        destinations: topLevelDestinations,
+        selectedIndex: selectedIndex,
+        onDestinationSelected: updateSelectedIndex,
+        backgroundColor: Colors.brown[300],
+        indicatorColor: Colors.brown,
+      ),
+    );
+  }
+}
