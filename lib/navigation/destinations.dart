79cbd94dc15c0084673e15590df3fa3a32ce7bac
Akhil Jose <akhiljose@gmail.com>
2024-10-04 17:23:38 +0530

    stores destinations for navigationBar

---
+++ lib/navigation/destinations.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const topLevelDestinationNames = ['Notes', 'Diary', 'Checklist'];
+
+const topLevelDestinations = [
+  NavigationDestination(icon: Icon(Icons.house), label: 'Notes'),
+  NavigationDestination(icon: Icon(Icons.house), label: 'Diary'),
+  NavigationDestination(icon: Icon(Icons.house), label: 'Checklist'),
+];
